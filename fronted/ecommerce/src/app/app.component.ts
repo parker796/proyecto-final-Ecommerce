b5ba57dc50ca31490f0ad1cea3af73b298ccd78c
parker796 <katch796@hotmail.com>
2023-02-20 18:43:11 -0600

    agregamos la plantilla de rafcart adaptada a nuestro proyecto en angular

--- fronted/ecommerce/src/app/app.component.ts
+++ fronted/ecommerce/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+declare function initPageEcommerce([]):any;
+//truco para que funione bien el app.js en nuestra plantilla
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce';
+
+  //puede ser en el constructor o en ciclo de vida de angular init para que recargue
+  //los archivos jquery
+  constructor(){
+    setTimeout(() => {
+      initPageEcommerce($); //esperamos a que se recargue todos las funciones que hay
+      //en el jquery para que nos funcione bien la plantilla
+    },50);
+  }
 }
